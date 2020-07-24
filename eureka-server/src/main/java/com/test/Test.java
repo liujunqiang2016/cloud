142eb84175f8cd8f3153637abf857a5916836acb
liujunqiang2016 <912887741@qq.com>
2020-07-24 20:22:42 +0800

    Update Test.java

--- eureka-server/src/main/java/com/test/Test.java
+++ eureka-server/src/main/java/com/test/Test.java
@@ -4,7 +4,8 @@ public class Test {
     public static void main(String[] args){
         System.out.println("aaaaaaaaaaaaaaaaaaaaabbbb");
         System.out.println("cccccccccccccccccccccccc");
-        
-        System.out.println("ddddddddddddddddddddddd");
+        System.out.println("dddddfdsfssdffdddddddddddddddddd");
+        System.out.println("dddddddddddsfdfdfdfdfdfddddddddddddd");
+        System.out.println("dddddddddddsfdfdfdfdfdfdddddddfdfdfdfddddddd");
     }
 }
