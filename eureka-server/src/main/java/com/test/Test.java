bc50dff050727c8fc00f88a211461b269a286ffc
liujunqiang2016 <912887741@qq.com>
2020-07-24 20:16:41 +0800

    Update Test.java

--- eureka-server/src/main/java/com/test/Test.java
+++ eureka-server/src/main/java/com/test/Test.java
@@ -3,6 +3,8 @@ package com.test;
 public class Test {
     public static void main(String[] args){
         System.out.println("aaaaaaaaaaaaaaaaaaaaabbbb");
-        System.out.println("cccccccccccccccccccccccc");
+        System.out.println("dfdfdfdf");
+        System.out.println("fdfdfdfdfd");
+        System.out.println("fdfdfdfdfdfdfdfdfdfd");
     }
 }
